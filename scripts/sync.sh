5158cc49aaba997b8c93fcb8d062da68f980cd11
Torsten Paul <Torsten.Paul@gmx.de>
2022-12-01 01:03:32 +0100

    Don't preserve permissions, keep server setting.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,4 +3,4 @@
 YEAR=${YEAR:-$(date +%Y)}
 BASE="/home/openscad/www/advent-calendar-${YEAR}"
 
-rsync -avP index.js $(grep ^setDay index.js | sed -e "s/^.*,\s*dir:\s*'\([^']*\).*$/\1/") openscad@files.openscad.org:"${BASE}"/
+rsync -rltvP --chmod=D700,F644 index.js $(grep ^setDay index.js | sed -e "s/^.*,\s*dir:\s*'\([^']*\).*$/\1/") openscad@files.openscad.org:"${BASE}"/
