70a85c52569e083d58c2d9a94e362f45769e2fe6
Torsten Paul <Torsten.Paul@gmx.de>
2022-12-04 04:57:32 +0100

    Update index.js to new format.

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
 /**/
 setDay(1, { author: 'Ulrich Bär', license: 'CC0', dir: 'Baubles',
-	urlImg: 'Baubles.png',
-	urlScad: 'Baubles.scad',
-	url3mf: 'Bauble4.3mf'
+	img: 'Baubles.png',
+	src: 'Baubles.scad',
+	bin: 'Bauble4.3mf'
 });
 setDay(2, { author: 'Ulrich Bär', license: 'CC0', dir: 'Blocks',
-	urlScad: 'Blocks.scad',
-	urlImg: 'Blocks.png'
+	img: 'Blocks.png',
+	src: 'Blocks.scad'
 });
 setDay(3, { author: 'Ulrich Bär', license: 'CC0', dir: 'City',
-	urlScad: 'City.scad',
-	urlImg: 'City.png'
+	img: 'City.png',
+	src: 'City.scad'
 });
 setDay(4, { author: 'Ulrich Bär', license: 'CC0', dir: 'Eyes',
-	urlScad: 'ExpressionEyes.scad',
-	urlImg: 'eyes.gif'
+	img: 'eyes.gif',
+	src: 'ExpressionEyes.scad'
 });
 setDay(5, { author: 'Ulrich Bär', license: 'CC0', dir: 'Curls',
-	urlScad: 'Curls.scad',
-	urlImg: 'Curls.png'
+	img: 'Curls.png',
+	src: 'Curls.scad'
 });
 setDay(6, { author: 'Ulrich Bär', license: 'CC0', dir: 'Diamond',
-	urlScad: 'Diamond.scad',
-	urlImg: 'Brilliant.png',
-	url3mf: 'Diamond-brilliant.3mf'
+	img: 'Brilliant.png',
+	src: 'Diamond.scad',
+	bin: 'Diamond-brilliant.3mf'
 });
 setDay(7, { author: 'Ryan A. Colyer', license: 'CC0', dir: 'BubbleTrap',
-	urlScad: 'bubble_trap.scad',
-	urlImg: 'bubble_trap.webp'
+	img: 'bubble_trap.webp',
+	src: 'bubble_trap.scad'
 });
 setDay(8, { author: 'Ulrich Bär', license: 'CC0', dir: 'Oblong',
-	urlScad: 'Oblong.scad',
-	urlImg: 'Oblong6565.png'
+	img: 'Oblong6565.png',
+	src: 'Oblong.scad'
 });
 setDay(9, { author: 'Ulrich Bär', license: 'CC0', dir: 'Track',
-	urlScad: 'Track.scad',
-	urlImg: 'Track.png',
-	url3mf: 'Track.3mf'
+	img: 'Track.png',
+	src: 'Track.scad',
+	bin: 'Track.3mf'
 });
 setDay(10, { author: 'Ulrich Bär', license: 'CC0', dir: 'Tree',
-	urlScad: 'Tree.scad',
-	urlImg: 'Tree.png'
+	img: 'Tree.png',
+	src: 'Tree.scad'
 });
 setDay(11, { author: 'Ulrich Bär', license: 'CC0', dir: 'Twister',
-	urlScad: 'Twister.scad',
-	urlImg: 'Twister.png',
-	url3mf: 'Twister.3mf'
+	img: 'Twister.png',
+	src: 'Twister.scad',
+	bin: 'Twister.3mf'
 });
 setDay(12, { author: 'Torsten Paul', license: 'CC0', dir: 'Hitomezashi',
-	urlScad: 'Hitomezashi.scad',
-	urlImg: 'Hitomezashi.png'
+	img: 'Hitomezashi.png',
+	src: 'Hitomezashi.scad',
+	info: 'See this <a href="https://youtu.be/JbfhzlMk2eY">Numberphile</a> video for a nice explanation how the pattern is created.'
 });
 setDay(13, { author: 'Ulrich Bär', license: 'CC0', dir: 'CubeFrames',
-	urlScad: 'CubeFrames.scad',
-	urlImg: 'CubeFrames3.png',
-	url3mf: 'CubeFrames.3mf'
+	img: 'CubeFrames3.png',
+	src: 'CubeFrames.scad',
+	bin: 'CubeFrames.3mf'
 });
 setDay(14, { author: 'Torsten Paul', license: 'CC0', dir: 'Mandelbrot',
-	urlScad: 'Mandelbrot.scad',
-	urlImg: 'Mandelbrot.png'
+	img: 'Mandelbrot.png',
+	src: 'Mandelbrot.scad'
 });
 setDay(15, { author: 'Ryan A. Colyer', license: 'CC0', dir: 'HappyGrass',
-	urlScad: 'happy_grass.scad',
-	urlImg: 'happy_grass.webp'
+	img: 'happy_grass.webp',
+	src: 'happy_grass.scad'
 });
 setDay(16, { author: 'Ulrich Bär', license: 'CC0', dir: 'Shells',
-	urlScad: 'Shells.scad',
-	urlImg: 'Shells.png',
-	url3mf: 'Shells.3mf' 
+	img: 'Shells.png',
+	src: 'Shells.scad',
+	bin: 'Shells.3mf'
 });
