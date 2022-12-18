0c222948d0c2da9ad911bc85fc007efd2b73f6d7
Torsten Paul <Torsten.Paul@gmx.de>
2022-12-18 21:56:34 +0100

    Update scripts and video.

--- Life/mkvideo.sh
+++ Life/mkvideo.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 set -e
 
-OPENSCAD="/home/jordan/openscad/modref+objects/b/openscad"
+OPENSCAD=${OPENSCAD:-openscad}
 
 usage() {
-    echo "Usage: $0 out pattern x,y offsetx,y generations speed" >&2
+    echo "Usage: $0 out pattern x,y offsetx,y generations" >&2
     exit 1
 }
 
 case $# in
-6)  ;;
+5)  ;;
 *)
     usage
     ;;
@@ -20,13 +20,13 @@ pattern=$2
 worldSize=$3
 offset=$4
 generations=$5
-speed=$6
 
-rm -f $out*.png
+mkdir -p png
+rm -f png/$out*.png
 
 "$OPENSCAD" \
-    -o $out.png \
-    --imgsize 500,500 \
+    -o png/$out.png \
+    --imgsize 900,900 \
     --animate $generations \
     --colorscheme DeepOcean \
     -D animate=true \
@@ -35,6 +35,3 @@ rm -f $out*.png
     -D "worldSize=[$worldSize];" \
     -D "offset=[$offset]" \
     Life.scad
-
-magick convert $out*.png -set delay $speed $out.gif
-
--- Life/mkvids.sh
+++ Life/mkvids.sh
@@ -1,10 +1,29 @@
 #! /bin/sh
 
-./mkvideo.sh block block 4,4 1,1 6 1x3
-./mkvideo.sh blinker blinker 5,5 1,2 10 1x3
-./mkvideo.sh glider glider 10,10 0,0 29 1x4
-./mkvideo.sh rpentomino rpentomino 40,40 19,19 30 1x7
-./mkvideo.sh gosperglidergun gosperglidergun 50,50 0,0 90 1x10
+repeat() {
+	CNT="$1"
+	BASE="$2"
+	for i in "png/$BASE"*
+	do
+		for n in $(seq 1 "$CNT")
+		do
+			echo "$i"
+		done
+	done
+}
 
-magick convert block.gif blinker.gif glider.gif rpentomino.gif gosperglidergun.gif all.gif
+# Running all in parallel!
+./mkvideo.sh block block 4,4 1,1 6 & \
+./mkvideo.sh blinker blinker 5,5 1,2 10 & \
+./mkvideo.sh glider glider 10,10 0,0 60 & \
+./mkvideo.sh rpentomino rpentomino 40,40 19,19 90 & \
+./mkvideo.sh gosperglidergun gosperglidergun 50,50 0,0 180
 
+# Debian: sudo apt install webp
+img2webp -v -loop 0 -d 50 -lossy -q 30 -m 4 \
+	$(repeat 10 block) \
+	$(repeat 10 blinker) \
+	$(repeat  5 glider) \
+	$(repeat  3 rpentomino) \
+	$(repeat  2 gosperglidergun) \
+	-o Life.webp
